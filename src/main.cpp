d51cd40e0fb1576e58986a793b8f55557e046d55
Fedor <f.artemenkov@rtgsoft.ru>
2024-12-06 15:33:04 +0300

    Prevent running app twice while debugging via XCode 16.1

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,15 @@
 
 #include "Application.h"
+#include <unistd.h>
 
 int main()
 {
+    
+    // Prevent running app twice while debugging via xcode 16.1
+#ifdef __APPLE__
+    sleep(1);
+#endif
+    
     Application app;
     app.run();
     
